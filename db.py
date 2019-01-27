519d579b9e82defc7cc9337cbe897c01acb87533
Ahmedija <36454914+Ahmedija@users.noreply.github.com>
2019-01-27 14:48:31 +0100

    Final version of python code

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 from mysql.connector import MySQLConnection, Error, cursor
-
 import mysql.connector
+
 
 #connection to database
 def Connect():
@@ -24,11 +24,10 @@ def Insert(row):
     cursor = connection.cursor()
     cursor.execute(query, val)
 
-
     connection.commit()
     cursor.close()
 
-    print("Entry success.")
+    print("\n+++++++Entry success++++++++ \n \n")
 
 #Fetching data from Database
 def Fetch():
@@ -41,11 +40,12 @@ def Fetch():
 
     calendar = cursor.fetchall()
 
-    print ("\nPrinting each row's column values \n \n")
+
+    print ("\nPrinting each row's column values in format YYYY-MM-DD: \n \n")
     for row in calendar:
-       print("Date         = ", row[1], )
-       print("Category     = ", row[2])
-       print("Description  = ", row[3], "\n")
+        print("Date         = ", row[1], )
+        print("Category     = ", row[2])
+        print("Description  = ", row[3], "\n")
     cursor.close()
 
 #Editing old date
@@ -57,6 +57,47 @@ def Edit(row):
     mycursor = connection.cursor()
 
     connection.commit()
+
+
+#Sorting list by date buble sort
+def sort_by_date():
+
+    mySQLconnection = Connect()
+    sqlSelectQuery = "select * from calendar"
+
+    cursor = mySQLconnection .cursor()
+    cursor.execute(sqlSelectQuery)
+
+    #Tuple calendar
+    calendar = cursor.fetchall()
+
+    #List calendar
+    calendarData = [list(row) for row in calendar]
+
+    sub_li = calendarData
+
+
+    #bubble sort
+    def Sort(sub_li):
+         l = len(sub_li)
+         for i in range(0, l):
+             for j in range(0, l-i-1):
+                 if (sub_li[j][1] > sub_li[j + 1][1]):
+                     tempo = sub_li[j]
+                     sub_li[j]= sub_li[j + 1]
+                     sub_li[j + 1]= tempo
+         return sub_li
+
+    sortedList = Sort(sub_li)
+
+    #Printing sorted list
+    print("Entries sorted by date: \n")
+    for row in sortedList:
+        print("Date         = ", row[1], )
+        print("Category     = ", row[2])
+        print("Description  = ", row[3], "\n \n")
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,56 +1,104 @@
 from datetime import date
 import db
 import record
-
-
-def welcome():
-    print("Wellcome to my program Study calendar \n")
-    print("Please select the folowing below: ")
-    print("1. Make new entry")
-    print("2. Edit date")
-    print("3. Print my entries")
-    print("4. List by date\n \n")
-
-    #1 ubaci u bazu
-def new_entry():
-    datum = str(date.today())
-    category = input("Enter category: ")
-    description = input("Description: ")
-    objekat = record.Record(datum, category, description)
-    db.Insert(objekat)
-
-    #2 edit entry
-def edit_entry():
-    dateEdited = input("Enter date you want to edit in format YYYY-MM-DD: ")
-    categotyEdited = input("Enter category you want to edit: ")
-    descriptionEdited = input("Enter description you want to edit: ")
-    objectEdited = record.Record(dateEdited, categotyEdited, descriptionEdited)
-    db.Insert(objectEdited)
-
-    #3 print entrys from database
-def list_entry():
-    db.Fetch()
-
-    #4
-def list_by_date(self):
-    print("Enter date")
-    date = 0
+import datetime
 
 
 
+def restart():
 
-welcome()
+
+    def welcome():
+
+        print("Wellcome to my program Study calendar \n")
+        print("Please select the following: ")
+        print("1. Make new entry for today")
+        print("2. Make entry for some date")
+        print("3. Print my entries")
+        print("4. Print sorted entries")
+        print("5. --------EXIT--------\n \n")
 
 
-num = int(input("Your selection: "))
+        #1 Make new entry
+    def new_entry():
+        datee = str(date.today())
+        category = input("Enter category: ")
+        description = input("Description: ")
 
-if num == 1:
-    new_entry()
-elif num ==2:
-    edit_entry()
-elif num==3:
-    list_entry()
-elif num==4:
-    list_by_date()
-else:
-    print("Please enter number from 1 to 4")
+        newEntryData = record.Record(datee, category, description)
+        db.Insert(newEntryData)
+
+        restart()
+
+        #2 Edit entry
+    def edit_entry():
+
+        #validation of date
+        inputDate = input("Enter the date in format 'YYYY-MM-DD': ")
+        year,month,day = inputDate.split('-')
+
+        isValidDate = True
+
+        try :
+
+            datetime.datetime(int(year),int(month),int(day))
+
+        except ValueError :
+
+            isValidDate = False
+
+        if(isValidDate) :
+
+            print ("\nInput date is VALID ...\n")
+            inputCategoty = input("Enter new category: ")
+            inputDescription = input("Enter new description: ")
+
+            objectEdited = record.Record(inputDate, inputCategoty, inputDescription)
+            db.Insert(objectEdited)
+
+        else:
+            print ("\nInput date is NOT VALID... \n")
+            edit_entry()
+
+        welcome()
+
+
+        #3 Print entrys from database
+    def list_entry():
+        db.Fetch()
+        restart()
+
+        #4 Sort by date
+    def sort_by_date():
+        db.sort_by_date()
+        restart()
+
+
+    #first selection
+    welcome()
+
+    #validation of user input
+    while True:
+        try:
+            num = int(input("Your selection 1 to 5: "))
+
+            if num == 1:
+                new_entry()
+            elif num ==2:
+                edit_entry()
+            elif num==3:
+                list_entry()
+            elif num==4:
+               sort_by_date()
+            elif num == 5:
+                exit()
+        except ValueError :
+
+
+            print("\nPlease enter number from 1 to 5: \n")
+            restart()
+            break
+
+
+restart()
+
